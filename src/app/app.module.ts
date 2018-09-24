9fc8b4e39cd47aa7d1c630e53d1c4a862b2eb70a
Gustavo M. Wulff <gustavo.wulff@chubb.com>
2018-09-24 13:15:11 -0500

    citofono

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,
@@ -54,7 +55,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
         appRoutes,
         { enableTracing: false } // <-- debugging purposes only
-    )
+    ),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/citofono/citofono.component.ts
+++ src/app/citofono/citofono.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var Peer: any;
 
 @Component({
   selector: 'app-citofono',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitofonoComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('myvideo') myvideo: ElementRef;
+
+
+  peer;
+  mypeerid;
+  anotherid;
+
+  constructor() {
+    this.peer = new Peer(null, null);
+    console.log(this.peer);
+  }
 
   ngOnInit() {
+    this.peer.on('open', id => {
+      this.mypeerid = id;
+      console.log('id', id);
+    });
+    const n = <any>navigator;
+    const video = this.myvideo.nativeElement;
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+
+    this.peer.on('call', call => {
+      n.getUserMedia({video: true, audio: true}, stream => {
+        call.answer(stream);
+        call.on(stream, remotestream => {
+          video.src = URL.createObjectURL(remotestream);
+          video.play();
+        });
+      }, error => {
+        console.log('Failed to get stream', error);
+      });
+    });
+  }
+
+  makeCall() {
+    const n = <any>navigator;
+    const video = this.myvideo.nativeElement;
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+
+    n.getUserMedia({video: true, audio: true}, (stream) => {
+      const call = this.peer.call(this.anotherid, stream);
+      call.on(stream, remotestream => {
+        video.src = URL.createObjectURL(remotestream);
+        video.play();
+      });
+    }, error => {
+      console.log('Failed to get local stream', error);
+    });
+
   }
 
 }
